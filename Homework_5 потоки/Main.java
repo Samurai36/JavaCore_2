cf636dc66424174749a70135e71eeb5db0cd041e
K.Viktor <svry88@gmail.com>
2020-12-13 22:22:54 +0300

    add Homework 5 threads

---
+++ Homework_5 потоки/Main.java
@@ -0,0 +1,110 @@
+package Viktor;
+/*Необходимо написать два метода, которые делают следующее:
+1) Создают одномерный длинный массив, например:
+static final int size = 10000000;
+static final int h = size / 2;
+float[] arr = new float[size];
+2) Заполняют этот массив единицами;
+3) Засекают время выполнения: long a = System.currentTimeMillis();
+4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
+arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+5) Проверяется время окончания метода System.currentTimeMillis();
+6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a);
+Отличие первого метода от второго:
+Первый просто бежит по массиву и вычисляет значения.
+Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один.
+
+Пример деления одного массива на два:
+System.arraycopy(arr, 0, a1, 0, h);
+System.arraycopy(arr, h, a2, 0, h);
+
+Пример обратной склейки:
+System.arraycopy(a1, 0, arr, 0, h);
+System.arraycopy(a2, 0, arr, h, h);
+
+Примечание:
+System.arraycopy() копирует данные из одного массива в другой:
+System.arraycopy(массив-источник, откуда начинаем брать данные из массива-источника, массив-назначение, откуда начинаем записывать данные в массив-назначение, сколько ячеек копируем)
+По замерам времени:
+Для первого метода надо считать время только на цикл расчета:
+for (int i = 0; i < size; i++) {
+arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+}
+Для второго метода замеряете время разбивки массива на 2, просчета каждого из двух массивов и склейки.
+ */
+
+public class Main {
+    static final int size = 10000000;
+    static final int h = size / 2;
+
+    public static void main(String[] args) {
+
+        metod1();
+        metod2();
+
+    }
+
+    static void metod1() {
+        float[] arr = new float[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println((System.currentTimeMillis() - a) + "мс - время выполнения первого метода");
+    }
+
+    static void metod3(float arr[]) {
+        synchronized (new Object()) {
+            for (int i = 0; i < h; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        }
+    }
+
+    static void metod2() {
+        float[] arr = new float[size];
+        float[] arr2 = new float[h];
+        float[] arr3 = new float[h];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, arr2, 0, h);
+        System.arraycopy(arr, h, arr3, 0, h);
+        for (int i = 0; i < h; i++) {
+            arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        for (int i = 0; i < h; i++) {
+            arr3[i] = (float) (arr3[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        Thread myThready = new Thread(new Runnable() {
+            public void run() {
+                metod3(arr2);
+            }
+        });
+
+        Thread myThready2 = new Thread(new Runnable() {
+            public void run() {
+                metod3(arr3);
+            }
+        });
+
+        myThready.start();
+        myThready2.start();
+
+        System.arraycopy(arr2, 0, arr, 0, h);
+        System.arraycopy(arr3, 0, arr, h, h);
+
+        System.out.println((System.currentTimeMillis() - a) + "мс - время выполнения второго метода");
+    }
+
+}
